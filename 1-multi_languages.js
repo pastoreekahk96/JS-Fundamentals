a763799ce65e23f78329198cde559cee07504b56
apastoreekahk96@gmail.com <apastoreekahk96@gmail.com>
2025-11-08 19:44:15 +0000

    Add task 2 1-multi_languages.js and update readme.

---
+++ 1-multi_languages.js
@@ -0,0 +1,5 @@
+let clan = "C"; let pyt = "Python"; let jS ="JavaScript";
+
+console.log(clan, "is fun");
+console.log(pyt, "is cool");
+console.log(jS, 'is amazing');
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # 0. First constant, first print
 Write a script that prints “JavaScript is amazing”:
+
+# 1. 3 languages
+Write a script that prints 3 lines:
