81467fc6f0a65b1c4c076cbfdc28a459921d54d0
apastoreekahk96@gmail.com <apastoreekahk96@gmail.com>
2025-11-10 07:51:22 +0000

    Edit the code and correct errors

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 const program_lang = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
-for(let lang = 0; lang < program_lang.length; lang++){
-       console.log(`${program_lang[lang]}`);
+for (let lang = 0; lang < program_lang.length; lang++){
+    console.log(`${program_lang[lang]}`);
 }
